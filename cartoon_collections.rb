ab393ba50fbccb25ae3f7fb9c78c57f4920de019
Dwight Nodolf <dnodolf@gmail.com>
2020-04-23 05:24:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(array)
   cheeses = ["cheddar", "gouda", "camembert"]
   
   
-  return array.find{ |queso| array.include?(cheeses)}
+  return array.find{ |queso| cheese.include?(queso)}
    
  #find the first cheese in the cheeses array and return it if it is true that the cheeses array is in the argument array
 end
